d9e60b6513a36540c4fb991e8dfc3e2d6d7008be
mtp124 <44953543+mtp124@users.noreply.github.com>
2018-11-11 20:38:28 -0800

    Testing app updated after install Heroku CLI.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Test Application
+            Testing Heroku App
           </a>
         </header>
       </div>
