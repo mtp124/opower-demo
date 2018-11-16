012e4b870b1cad338596bde8db8eafefa24aea0a
mtp124 <44953543+mtp124@users.noreply.github.com>
2018-11-15 23:41:33 -0800

    Added additional styling and pane toggle JS functionality.

--- src/App.js
+++ src/App.js
@@ -10,53 +10,65 @@ import Button from '@material-ui/core/Button';
 import { Grid, Paper, MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import blue from '@material-ui/core/colors/blue';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-const Header = (props) => {
-  return (
-    <h2>{props.title}</h2>
-  )
-}
+//Custom components
+import Chart from './Electricity-Savings/Chart';
 
+
+//Button Icons
 const CarIcon = () => {
   return <img src={Car} alt="Car" style={{width: "30px", marginRight: "8px"}}/>
 }
-
 const TreeIcon = () => {
   return <img src={Tree} alt="Tree" style={{width: "30px", marginRight: "8px"}}/>
 }
-
 const CarbonIcon = () => {
   return <img src={Co2} alt="Co2" style={{width: "30px", marginRight: "8px"}}/>
 }
 
+//Pane Headers
 const Results = (props) => {
   return (<div>
     <h2>{props.savings}</h2>
-    <img src={Cars} alt="Cars" style={{width: "100%"}}/>
-
     </div>
   )
 }
-
 const Graph = (props) => {
   return (
     <h2>{props.graph}</h2>
   )
 }
-
+//Pane Content
 class App extends Component {
   state = {
-    name: ' '
+    leftPaneDefault: <img src={Cars} alt="Cars" style={{width: "100%"}}/>,
+    rightPaneDefault: <Chart/>
   }
 
-  changeName = (newName) => {
+  carsSavings = () => {
     this.setState({
-      name: newName
+      leftPaneDefault: <img src={Car} alt="Car" style={{width: "100%"}}/>,
+      rightPaneDefault: <Chart/>
+    });
+  }
+
+  treesSavings = () => {
+    this.setState({
+      leftPaneDefault: <img src={Co2} alt="Co2" style={{width: "100%"}}/>,
+      rightPaneDefault: <img src={Tree} alt="Tree" style={{width: "100%"}}/>
+    });
+  }
+
+  carbonSavings = () => {
+    this.setState({
+      leftPaneDefault: <img src={Car} alt="Car" style={{width: "100%"}}/>,
+      rightPaneDefault: <img src={Co2} alt="Co2"style={{width: "100%"}}/>
     });
   }
 
   render() {
-
     const theme = createMuiTheme({
       palette: {
         primary: blue
@@ -64,25 +76,23 @@ class App extends Component {
       typography: {
         useNextVariants: true,
         }
-
     });
 
     console.log(theme);
 
+    //Custom Styles
     const appContainerStyle = {
       //border: "2px solid lightGray",
       borderRadius: "10px",
-      margin: "0 auto",
+      margin: "20px auto 0 auto",
       padding: "10px 40px 30px 40px",
       maxWidth: "1000px"
     }
-
     const headerStyle = {
       color: "lightBlue",
-      borderBottom: "2px solid lightGray",
+      //borderBottom: "2px solid lightGray",
       textAlign: "left"
     }
-
     const buttonContainerStyles = {
        textAlign: "center",
        marginTop: "20px"
@@ -92,34 +102,38 @@ class App extends Component {
     <MuiThemeProvider theme={theme}>
       <Paper elevation={8} style={appContainerStyle}>
         <Grid container spacing={16}>
-        <Grid item xs={12} style={headerStyle}>
-         
-        <Typography component="h5" variant="h5" gutterBottom>
-          Your Electricity Savings
-        </Typography>
-      </Grid>
+          <Grid item xs={12} style={headerStyle}>
+          <AppBar position="static" color="default">
+           <Toolbar>
+            <Typography component="h5" variant="h5" gutterBottom>
+            Your Electricity Savings
+            </Typography>
+          </Toolbar>
+        </AppBar>   
+       </Grid>
       <Grid item sm className="animated bounceInLeft">
         <Paper elevation={5}>
           <Results savings="Hello"/>
+          <div>{this.state.leftPaneDefault}</div>
         </Paper>
-          <div style={buttonContainerStyles}>
-
-            <Button style={{borderTopRightRadius: "0", borderBottomRightRadius: "0"}} onClick={this.changeName.bind(this, <Results/>)} className="animated bounce" variant="contained" color="inherit" size="large">
-                <CarIcon/>cars
+          
+        <div style={buttonContainerStyles}>
+          <Button style={{borderTopRightRadius: "0", borderBottomRightRadius: "0"}} onClick={() => this.carsSavings((<img src={Car} alt="Car" style={{width: "100%"}}/>))} className="animated bounce" variant="contained" color="inherit" size="large">
+              <CarIcon/>cars
             </Button>
-            <Button style={{borderRadius: "0"}} onClick={() => this.changeName('TREES')} className="animated bounce" variant="contained" color="inherit" size="large">
+              <Button style={{borderRadius: "0"}} onClick={() => this.treesSavings((<img src={Tree} alt="Tree" style={{width: "100%"}}/>))} className="animated bounce" variant="contained" color="inherit" size="large">
                 <TreeIcon/>trees
-            </Button>
-            <Button style={{borderTopLeftRadius: "0", borderBottomLeftRadius: "0"}} onClick={() => this.changeName('CARBON')} className="animated bounce" variant="contained" color="inherit" size="large">
-                <CarbonIcon/>carbon
+              </Button>
+            <Button style={{borderTopLeftRadius: "0", borderBottomLeftRadius: "0"}} onClick={() => this.carbonSavings('CARBON')} className="animated bounce" variant="contained" color="inherit" size="large">
+              <CarbonIcon/>carbon
           </Button>
-
+         
         </div>
       </Grid>
       <Grid item sm className="animated bounceInRight">
         <Paper elevation={5}>
           <Graph graph="There"/>
-          <div>{this.state.name}</div>
+          <div>{this.state.rightPaneDefault}</div>
 
         </Paper>
       </Grid>
---
+++ src/Electricity-Savings/Chart.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Chart } from "react-google-charts";
+ 
+const EnergyChart = () => {
+  return (
+    <div style={{marginLeft: "20px"}}>
+    <Chart 
+  width={'400px'}
+  height={'300px'}
+  chartType="Bar"
+  loader={<div>Loading Chart</div>}
+  data={[
+    ['Year', 'Energy Usage', 'Savings'],
+    ['2015', 400, 200],
+    ['2016', 460, 250],
+    ['2017', 1120, 300],
+    ['2018', 540, 350],
+  ]}
+  options={{
+    // Material design options
+    chart: {
+      title: 'Last Month, You Saved Enough Energy To Take Six Cars Off The Road!',
+      subtitle: 'Great job! You used 573 KWH last month compared to 791 KWH in the same month last year. The resulting savings is enought to offset the energy consumption of six average-size cars for one month.',
+    },
+  }}
+  // For tests
+  rootProps={{ 'data-testid': '2' }}
+/></div>
+  );
+};
+export default EnergyChart;
